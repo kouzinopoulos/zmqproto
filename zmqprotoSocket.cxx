5a6fb772e7e6751a1d9bf4e07522c79403a5ef13
chkouzin <charalampos.kouzinopoulos@cern.ch>
2014-05-27 14:16:44 +0200

    Ported the generic ZeroMQ socket interface from FairRoot as a zmqprotoSocket/zmqprotoContext interface

---
+++ zmqprotoSocket.cxx
@@ -0,0 +1,144 @@
+#include <iostream>
+#include <sstream>
+
+#include "zmqprotoSocket.h"
+
+using namespace std;
+
+zmqprotoSocket::zmqprotoSocket (const string& type, int num) :
+  fBytesTx(0),
+  fBytesRx(0),
+  fMessagesTx(0),
+  fMessagesRx(0)
+{
+  fContext = zmq_ctx_new ();
+  if (fContext == NULL){
+    cout << "failed creating context, reason: " << zmq_strerror(errno) << endl;
+  }
+  
+  stringstream id;
+  id << type << "." << num;
+  fId = id.str();
+
+  fSocket = zmq_socket(fContext, GetConstant(type));
+
+  int rc = zmq_setsockopt(fSocket, ZMQ_IDENTITY, &fId, fId.length());
+  if (rc != 0) {
+    cout << "failed setting socket option, reason: " << zmq_strerror(errno) << endl;
+  }
+  
+  if (type == "sub") {
+    rc = zmq_setsockopt(fSocket, ZMQ_SUBSCRIBE, NULL, 0);
+    if (rc != 0) {
+      cout << "failed setting socket option, reason: " << zmq_strerror(errno) << endl;
+    }
+  }
+
+  cout << "created socket #" << fId << endl;
+}
+
+unsigned long zmqprotoSocket::GetBytesTx()
+{
+  return fBytesTx;
+}
+
+unsigned long zmqprotoSocket::GetBytesRx()
+{
+  return fBytesRx;
+}
+
+unsigned long zmqprotoSocket::GetMessagesTx()
+{
+  return fMessagesTx;
+}
+
+unsigned long zmqprotoSocket::GetMessagesRx()
+{
+  return fMessagesRx;
+}
+
+void zmqprotoSocket::Close()
+{
+  if (fSocket == NULL){
+    return;
+  }
+
+  int rc = zmq_close (fSocket);
+  if (rc != 0) {
+    cout << "failed closing socket, reason: " << zmq_strerror(errno) << endl;
+  }
+
+  fSocket = NULL;
+}
+
+void* zmqprotoSocket::GetSocket()
+{
+  return fSocket;
+}
+
+size_t zmqprotoSocket::Send(zmq_msg_t *msg, const string& flag)
+{
+  int nbytes = zmq_msg_send (msg, fSocket, GetConstant(flag));
+  if (nbytes >= 0){
+    fBytesTx += nbytes;
+    ++fMessagesTx;
+    return nbytes;
+  }
+  if (zmq_errno() == EAGAIN){
+    return false;
+  }
+  cout << "failed sending on socket #" << fId << ", reason: " << zmq_strerror(errno) << endl;
+  return nbytes;
+  
+}
+
+size_t zmqprotoSocket::Receive(zmq_msg_t *msg, const string& flag)
+{
+  int nbytes = zmq_msg_recv (msg, fSocket, GetConstant(flag));
+  if (nbytes >= 0){
+    fBytesRx += nbytes;
+    ++fMessagesRx;
+    return nbytes;
+  }
+  if (zmq_errno() == EAGAIN){
+    return false;
+  }
+  cout << "failed receiving on socket #" << fId << ", reason: " << zmq_strerror(errno) << endl;
+  return nbytes;
+}
+
+void zmqprotoSocket::Bind(const string& address)
+{
+  cout << "bind socket " << fId << " on " << address << endl;
+
+  int rc = zmq_bind (fSocket, address.c_str());
+  if (rc != 0) {
+    cout << "failed binding socket #" << fId << ", reason: " << zmq_strerror(errno) << endl;
+  }
+}
+
+void zmqprotoSocket::Connect(const string& address)
+{
+  cout << "connect socket #" << fId << " on " << address << endl;
+
+  int rc = zmq_connect (fSocket, address.c_str());
+  if (rc != 0) {
+    cout << "failed connecting socket #" << fId << ", reason: " << zmq_strerror(errno) << endl;
+  }
+}
+
+int zmqprotoSocket::GetConstant(const string& constant)
+{
+  if (constant == "") return 0;
+  if (constant == "sub") return ZMQ_SUB;
+  if (constant == "pub") return ZMQ_PUB;
+  if (constant == "xsub") return ZMQ_XSUB;
+  if (constant == "xpub") return ZMQ_XPUB;
+  if (constant == "push") return ZMQ_PUSH;
+  if (constant == "pull") return ZMQ_PULL;
+  if (constant == "snd-hwm") return ZMQ_SNDHWM;
+  if (constant == "rcv-hwm") return ZMQ_RCVHWM;
+  if (constant == "snd-more") return ZMQ_SNDMORE;
+  if (constant == "rcv-more") return ZMQ_RCVMORE;
+  return -1;
+}
---
+++ zmqprotoSocket.h
@@ -0,0 +1,40 @@
+#ifndef ZMQPROTOSOCKET_H
+#define ZMQPROTOSOCKET_H
+
+#include <cstdlib>
+#include <string>
+
+#include <zmq.h>
+
+class zmqprotoSocket
+{
+  public:
+    zmqprotoSocket(const std::string& type, int num);
+    
+    virtual unsigned long GetBytesTx();
+    virtual unsigned long GetBytesRx();
+    virtual unsigned long GetMessagesTx();
+    virtual unsigned long GetMessagesRx();
+    
+    virtual size_t Receive(zmq_msg_t *msg, const std::string& flag);
+    virtual size_t Send(zmq_msg_t *msg, const std::string& flag);
+    
+    virtual void Bind(const std::string& address);
+    virtual void Connect(const std::string& address);
+    
+    virtual void* GetSocket();
+    virtual void Close();
+     
+  private:
+    void* fContext;
+    void* fSocket;
+    std::string fId;
+    unsigned long fBytesTx;
+    unsigned long fBytesRx;
+    unsigned long fMessagesTx;
+    unsigned long fMessagesRx;
+    
+    int GetConstant(const std::string& constant);
+};
+
+#endif
